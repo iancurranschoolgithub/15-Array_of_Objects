192420bd70d05ca937f5066cbeb851841c1d829a
iancurranschoolgithub <inac2004@yahoo.com>
2022-03-17 14:11:32 -0400

    finsihed Monster class

--- src/MonsterMash/Monster.java
+++ src/MonsterMash/Monster.java
@@ -9,10 +9,44 @@ import static java.lang.Math.*;
 import static java.util.Arrays.*;
 import static java.util.Collections.*;
 
-public class Monster
-{
-	//build your own Monster class 
-	//with methods like isBigger and isSmaller
-	//or use your Monster class from
-	//lab18 lab set 1
+public class Monster {
+    private int height, weight, age;
+
+    public Monster(int h, int w, int a) {
+        height = h;
+        weight = w;
+        age = a;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String toString() {
+        return "" + height + " " + weight + " " + age;
+    }
+
+    public boolean isBigger(int n) {
+        boolean bb = n < height;
+        return bb;
+    }
+
+    public boolean isSmaller(int m) {
+        boolean bs = m > height;
+        return bs;
+    }
+
+
+    //build your own Monster class
+    //with methods like isBigger and isSmaller
+    //or use your Monster class from
+    //lab18 lab set 1
 }

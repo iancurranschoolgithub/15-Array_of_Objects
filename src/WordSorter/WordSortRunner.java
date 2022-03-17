ca9e57001750b268a2b9957744ab5730f0beb15c
iancurranschoolgithub <inac2004@yahoo.com>
2022-03-17 13:01:43 -0400

    creating files pt 2

---
+++ src/WordSorter/WordSortRunner.java
@@ -0,0 +1,11 @@
+package WordSorter;//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class WordSortRunner
+{
+	public static void main(String args[])
+	{
+		//add test cases
+	}
+}
---
+++ src/WordSorter/WordSortTwo.java
@@ -0,0 +1,22 @@
+package WordSorter;//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class WordSortTwo
+{
+	private String[] wordRay;
+
+	public WordSortTwo(String sentence)
+	{
+	}
+
+	public void sort()
+	{
+	}
+
+	public String toString()
+	{
+		String output="";
+		return output+"\n\n";
+	}
+}
